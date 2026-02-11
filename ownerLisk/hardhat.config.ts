66b6109c325f039e377eab24735c70c1ea06457e
halima <dahunsiolajumoke18@gmail.com>
2026-02-11 02:59:29 +0100

    assignment completed

---
+++ ownerLisk/hardhat.config.ts
@@ -0,0 +1,25 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
+
+const { LISK_SEPOLIA_URL, PRIVATE_KEY, ARC_TESTNET_URL } = process.env;
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+  networks: {
+    liskSepolia: {
+      url: LISK_SEPOLIA_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+      chainId: 4202,
+    },
+    arcTestnet: {
+      url:ARC_TESTNET_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+      chainId: 5042002,
+    }
+
+  },
+};
+
+export default config;
+
